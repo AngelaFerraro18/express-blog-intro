10c456a176efdd0177bfaaec0bed5e23f3416c5a
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-26 14:42:37 +0100

    feat: create route "bacheca"

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//creo la route della homepage usando / 
 app.get('/', (req, res) =>{
 
     //mando la risposta con un messaggio per il client
     res.send('Server del mio blog');
 })
 
+//creo la route per la bacheca del blog
+app.get('/bacheca', (req, res) =>{
+
+    //mando la risposta con un messaggio per il client
+    res.send('bacheca');
+})
+
 app.listen(port, () =>{
     console.log(`Il server è in ascolto alla porta: ${port}.`)
 })
+
