a20caf63ed02e7fc568c06884a85710a099a8c59
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-26 14:58:39 +0100

    feat: create posts list with array/obj

---
+++ data/list-posts.js
@@ -0,0 +1,35 @@
+//creo un array di oggetti che rappresenterà la lista dei post per la bacheca
+
+let postsList = [
+    {
+        'title' : 'Ciambellone',
+        'content' : 'Il dolce classico semplice da preparare',
+        'image' : 'images/ciambellone.jpg',
+        'tags' : ['cake', 'cooking', 'bakery']
+    },
+    {
+        'title' : 'Cracker di barbabietola',
+        'content' : 'Snack sano e croccante',
+        'image' : 'images/cracker_barbabietola.jpg',
+        'tags' : ['crunchy', 'cooking', 'bakery']
+    },
+    {
+        'title' : 'Pane fritto dolce',
+        'content' : 'Al pane fritto non puoi dire di no',
+        'image' : 'images/pane_fritto_dolce.jpg',
+        'tags' : ['bread', 'cooking', 'bakery']
+    },
+    {
+        'title' : 'Pasta alla barbabietola',
+        'content' : 'Un tocco colorato per un piatto divertente',
+        'image' : 'images/pasta_barbabietola.jpg',
+        'tags' : ['pasta', 'cooking', 'bakery']
+    },
+    {
+        'title' : 'Torta paesana',
+        'content' : 'Il dolce più antico del mondo',
+        'image' : 'images/torta_paesana',
+        'tags' : ['cake', 'cooking', 'bakery']
+    }
+   
+]
