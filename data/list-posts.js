5264a9e9ecdbc324b69936330aade29e88f59809
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-26 15:23:18 +0100

    fix: correction name image format

--- data/list-posts.js
+++ data/list-posts.js
@@ -4,31 +4,31 @@ let postsList = [
     {
         'title' : 'Ciambellone',
         'content' : 'Il dolce classico semplice da preparare',
-        'image' : 'images/ciambellone.jpg',
+        'image' : 'images/ciambellone.jpeg',
         'tags' : ['cake', 'cooking', 'bakery']
     },
     {
         'title' : 'Cracker di barbabietola',
         'content' : 'Snack sano e croccante',
-        'image' : 'images/cracker_barbabietola.jpg',
+        'image' : 'images/cracker_barbabietola.jpeg',
         'tags' : ['crunchy', 'cooking', 'bakery']
     },
     {
         'title' : 'Pane fritto dolce',
         'content' : 'Al pane fritto non puoi dire di no',
-        'image' : 'images/pane_fritto_dolce.jpg',
+        'image' : 'images/pane_fritto_dolce.jpeg',
         'tags' : ['bread', 'cooking', 'bakery']
     },
     {
         'title' : 'Pasta alla barbabietola',
         'content' : 'Un tocco colorato per un piatto divertente',
-        'image' : 'images/pasta_barbabietola.jpg',
+        'image' : 'images/pasta_barbabietola.jpeg',
         'tags' : ['pasta', 'cooking', 'bakery']
     },
     {
         'title' : 'Torta paesana',
         'content' : 'Il dolce più antico del mondo',
-        'image' : 'images/torta_paesana',
+        'image' : 'images/torta_paesana.jpeg',
         'tags' : ['cake', 'cooking', 'bakery']
     }
    
